f859e001c33f1c9a7d8f4f7de6e9e9a86b8ccbce
Max <generalmaxis@yandex.ru>
2019-05-30 18:42:27 +0500

    1-ая версия

--- ChangeTime/Program.cs
+++ ChangeTime/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-namespace ChangeTime
+namespace DateChanger
 {
     class Program
     {
-        static void Main(string[] args)
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEMTIME
         {
+            public short wYear;
+            public short wMonth;
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            public short wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime([In] ref SYSTEMTIME st);
+
+        public static void Main(string[] args)
+        {
+            DateTime tmp = DateTime.UtcNow;
+            Console.WriteLine(tmp);
+            SYSTEMTIME st = new SYSTEMTIME();
+            st.wYear = Convert.ToInt16(DateTime.UtcNow.Year);
+            st.wMonth = Convert.ToInt16(1);
+            st.wDay = Convert.ToInt16(DateTime.UtcNow.Day);
+            st.wHour = Convert.ToInt16(DateTime.UtcNow.Hour);
+            st.wMinute = Convert.ToInt16(DateTime.UtcNow.Minute);
+            st.wSecond = Convert.ToInt16(DateTime.UtcNow.Second);
+            st.wMilliseconds = Convert.ToInt16(DateTime.UtcNow.Millisecond);
+            SetSystemTime(ref st);
+
+            // Process.Start("");
+            st.wYear = Convert.ToInt16(tmp.Year);
+            st.wMonth = Convert.ToInt16(tmp.Month);
+            st.wDay = Convert.ToInt16(tmp.Day);
+            st.wHour = Convert.ToInt16(tmp.Hour);
+            st.wMinute = Convert.ToInt16(tmp.Minute);
+            st.wSecond = Convert.ToInt16(tmp.Second);
+            st.wMilliseconds = Convert.ToInt16(tmp.Millisecond);
+            SetSystemTime(ref st);
+            Console.ReadKey();
         }
     }
 }
